6a659502e4db280f25feaf8fa13b871cc00424a5
nilamkj <nilam.bhogawale@gmail.com>
2024-04-12 10:39:15 +0530

    Add project files.

---
+++ Assessments/NewFolder/Display1to50.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessments.NewFolder
+{
+    public class Display1to50
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=50; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Assessments/NewFolder/GreatestNoBetween3No.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessments.NewFolder
+{
+    public class GreatestNoBetween3No
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            num1 = 20;
+            num2 = 40;
+            num3 = 80;
+            if(num1>num2)
+            {
+                if(num1>num3)
+                {
+                    Console.WriteLine("num1 is greatest");
+                }
+                else
+                {
+                    Console.WriteLine("num3 is greatest");
+                }
+            }
+            else if(num1>num3)
+            {
+                Console.WriteLine("num2 is greatest");
+            }
+            else
+            {
+                Console.WriteLine("num3 is greatest");
+            }
+        }
+    }
+}
